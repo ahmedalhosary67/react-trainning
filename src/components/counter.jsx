8536eb235b20e0376a072440467dd6e05ce4734d
Ahmed Alhosary <ahmedalhosary67@gmail.com>
2021-07-06 15:17:15 +0200

    first react training

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,38 +2,59 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.counter.value,
+        // tags: []
         // imageUrl: "https://picsum.photos/200"
     };
-
-
+    
+    handleIncrement = () => { // we use arrow function to bind (this) auto 
+        
+        this.setState({ value: this.state.value + 1 })
+        
+    }
+    
+    render() { 
+        // console.log('props', this.props.children);
+        
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.FormatCount()}</span> 
+                <button onClick={() => this.handleIncrement()} className="btn btn-secondary btn=sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+                {/* { this.renderTags() } */}
+            </div>
+        );
+    }
+    getBadgeClasses() {
+        let classess = "badge m-2 badge-";
+        classess += (this.state.value === 0) ? "warning" : "primary";
+        return classess;
+    }
+    
+    FormatCount() {
+        const { value: count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+    
     // styles = {
     //     fontSize: 50,
     //     fontWeight: "bold"
     // }
 
 
-    renderTags() {
-        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    }
+    // renderTags() {
+    //     if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    // }
     
     
     // constructor() {
         // super();
         // this.handleIncrement = this.handleIncrement.bind(this); // to allow to bind (this) inside the handle function => first step
         // }
-        // handleIncrement() {
-            //     console.log("Increment clicked", this);
-            // } ==> second step
-            
-            
-    handleIncrement = () => { // ti bind (this) auto we use arrow function 
-        
-        console.log("Increment clicked", this);
-    }
-
+    // handleIncrement() {
+        //     console.log("Increment clicked", this);
+        // } ==> second step
 
     //render() { 
         // let classess = this.getBadgeClasses();
@@ -52,27 +73,6 @@ class Counter extends Component {
         
     //     );
     // }
-
-
-    render() { 
-        return (
-        <div>
-            <span className={this.getBadgeClasses()}>{this.FormatCount()}</span> 
-            <button onClick={this.handleIncrement} className="btn btn-secondary btn=sm">Increment</button>
-            { this.renderTags() }
-        </div>
-        );
-    }
-    getBadgeClasses() {
-        let classess = "badge m-2 badge-";
-        classess += (this.state.count === 0) ? "warning" : "primary";
-        return classess;
-    }
-
-    FormatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
 }
  
 export default Counter;
